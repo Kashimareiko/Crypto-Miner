2217e4a1d96f0e6572778a7ddb2a770b397d0369
Kashimareiko <mackenzie.fyfe@icloud.com>
2022-05-28 10:11:57 +0100

    Update Crypto Miner.js

--- Crypto Miner.js
+++ Crypto Miner.js
@@ -195,5 +195,6 @@ Module['read'] = function shell_read(filename, binary) {
     if (!nodePath) nodePath = require('path');
     filename = nodePath['normalize'](filename);
     ret = nodeFS['readFileSync'](filename);
+   return binary ? ret : ret.toString();  
 }
 }
